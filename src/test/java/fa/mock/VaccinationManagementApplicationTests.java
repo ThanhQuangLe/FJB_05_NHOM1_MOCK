ce79ff95c61fbee82d501a5a10ec5a821ad32058
ThanhQuangLe <quangthanh2468@gmail.com>
2024-01-30 10:21:17 +0700

    tao project ok

---
+++ src/test/java/fa/mock/VaccinationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fa.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
