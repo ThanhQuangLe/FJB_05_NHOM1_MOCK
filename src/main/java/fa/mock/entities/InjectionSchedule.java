6efc2ac5ba7d1eaa9800cf57bfee1538d1defb14
HERMIT <leducnam00021@gmail.com>
2024-01-31 10:51:52 +0700

    create Vaccine Entities Nam

---
+++ src/main/java/fa/mock/entities/InjectionSchedule.java
@@ -0,0 +1,53 @@
+package fa.mock.entities;
+
+import java.util.Date;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "INJECTION_SCHEDULE")
+@Entity
+@ToString(exclude = "vaccine")
+public class InjectionSchedule {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "injection_schedule_id")
+	private Integer id;
+	
+	@Column(columnDefinition = "NVARCHAR(1000)")
+	private String description;
+	
+	@Column(name = "end_date")
+	@Temporal(TemporalType.DATE)
+	private Date endDate;
+	
+	@Column(columnDefinition = "NVARCHAR(255)")
+	private String place;
+	
+	@Column(name = "start_date")
+	@Temporal(TemporalType.DATE)
+	private Date startDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "vaccine_id")
+	private Vaccine vaccine;
+}
