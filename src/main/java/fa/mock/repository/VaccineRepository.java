14698505faafb8e6f14678581439bb4d4a44c943
QuanCV-HN <chudinhquan1801@gmail.com>
2024-02-20 10:32:41 +0700

    xong het

--- src/main/java/fa/mock/repository/VaccineRepository.java
+++ src/main/java/fa/mock/repository/VaccineRepository.java
@@ -4,7 +4,9 @@ import fa.mock.entities.Vaccine;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,5 +18,16 @@ public interface VaccineRepository extends JpaRepository<Vaccine,String> {
 
     @Query("SELECT v.image FROM Vaccine v WHERE v.id = ?1")
     byte[] getImageDataById(String id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Vaccine v SET v.status = false WHERE v.vaccineType.id = ?1")
+    Integer updateVaccinesStatusFalse (String vaccineTypeId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Vaccine v SET v.status = true WHERE v.vaccineType.id = ?1")
+    Integer updateVaccinesStatusTrue (String vaccineTypeId);
+
 
 }
